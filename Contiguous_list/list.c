19d0d7cd028c088094ec7bb8f1659bd7ec1c2f05
ianbandon <101912431+ianbandon@users.noreply.github.com>
2022-04-12 22:23:58 +0800

    Add files via upload

--- Contiguous_list/list.c
+++ Contiguous_list/list.c
@@ -9,41 +9,58 @@
 //Sqlist 是结构体的类型
 typedef struct Sqlist
 {
-	int* date; 
+	//数据数量最大值
 	int Maxsize;
+	//已存入的数据的个数
 	int length;
+	//数组的指针-->date
+	int(* date)[Initsize];
 }Sqlist;
+//int arr[Initsize];
+//int *date=&arr
+//
+
 //malloc 函数的参数    malloc(sizeif(Elemtype)*Insize) 
 //malloc 函数 返回一个指针  需要强制转换成你定义的数据元素类型指针
-//初始化数组
+     //初始化数组
 void Inlist(Sqlist* s)
 {
-	int i = 0;
-	s->length = 0;
-	s->date = (int*)malloc(sizeof(int)*Initsize);
-	s->Maxsize = Initsize;
+	//没有存数据所以 数据数量是0
+	 s->length = 0;
+	 s->Maxsize = Initsize;
+	//申请内存空间 10个int型
+	//malloc传过来的是整个数组的地址
+	 //p 为指针 指向是一个数组 三个元素int
+	s->date=malloc(sizeof(int)*Initsize);
+	
+	 int i = 0;
+	 for (i = 0; i < Initsize; i++)
+	 {
+		 *(s->date)[i] = 0;
+	 }
 }
 //动态增加数组的长度
 void IncreaseSize(Sqlist* s,int len)
-{
-	int 
-}
-
+{}
+//打印 数组
 void print(Sqlist* s)
 {
+	printf("L.length=%d\n", s->length);
+	printf("L.Maxsize=%d\n", s->Maxsize);
 	int i = 0;
-	for (i = 0; i < Maxsize; i++)
+	for (i = 0; i < Initsize; i++)
 	{
-		printf("%d ",s->date[i]);
+		printf("date[%d]=%d ",i,*(s->date)[i]);
 	}
 }
 int main()
 {
-	//定义一个顺序表
+	//定义一个顺序表  默认最大值是10个int型
 	Sqlist L;
 	//初始化顺序表
-	Inlist(&L，);
+	Inlist(&L);
 	//打印顺序表
 	print(&L);
+
 	return 0;
 }
